9e14d3cd1ae1a807889aa05293c1e7878da12fc8
seomin <>
2018-03-31 23:43:00 +0200

    refactored import_cards

--- deckbuilder-api/db/import_cards.py
+++ deckbuilder-api/db/import_cards.py
@@ -7,29 +7,37 @@ mci_base_url = "https://magiccards.info/scans/en/"
 
 
 def import_cards():
+    print("Dropping all cards...")
     dao.drop_cards()
 
-    # Load cards from json file
+    print("Loading cards from json file...")
     script_dir = os.path.dirname(__file__)
     rel_path = "../cards/AllSets-x.json"
     abs_file_path = os.path.join(script_dir, rel_path)
     file = open(abs_file_path, encoding="UTF-8")
     data = json.load(file)
 
-    # Save cards in database
+    print("Saving cards in database...")
     for set_name, set_data in data.items():
         set_cards = set_data["cards"]
-        mci_set = set_data.get("magicCardsInfoCode", set_name.lower())
+        mci_set = set_data.get("magicCardsInfoCode")
 
-        # TODO import cards even if they do not have number or mciNumber
         for card in set_cards:
-            if "mciNumber" not in card:
-                if "number" not in card:
-                    print("Card \"" + card["name"] + "\" in set " + set_name + " has neither number nor mciNumber. Ignoring...")
-                    continue
-                mci_number = card["number"]
-            else:
-                mci_number = card["mciNumber"]
+            # Only set mciUrl for cards whose set is available on MCI
+            if mci_set is not None:
+                mci_number = extract_mci_number(card)
+                if mci_number is not None:
+                    card["mciUrl"] = mci_base_url + mci_set + "/" + mci_number + ".jpg"
 
-            card["mciUrl"] = mci_base_url + mci_set + "/" + mci_number + ".jpg"
             dao.save_card(card)
+    print("Done.")
+
+
+def extract_mci_number(card):
+    if "mciNumber" in card:
+        return card["mciNumber"]
+
+    if "number" in card:
+        return card["number"]
+
+    return None
