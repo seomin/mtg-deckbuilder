a6ad0ee6a06e1a005665466ff8876d7b0b8d35e6
seomin <>
2018-04-14 20:13:32 +0200

    renamed test

--- deckbuilder-api/tests/mana_parser_test.py
+++ deckbuilder-api/tests/mana_parser_test.py
@@ -1,12 +1,12 @@
 import unittest
 
-from util import color_parser
+from util import mana_parser
 
 
 class TestStatistics(unittest.TestCase):
 
     def test_it(self):
-        cost = color_parser.parse("{3}{W}{B}{B}")
+        cost = mana_parser.parse("{3}{W}{B}{B}")
         self.assertEqual(1, cost["W"])
         self.assertEqual(2, cost["B"])
         self.assertEqual(3, cost["colorless"])
